7f414c51f73ddb376b01648de186ad3d5ab2b4d7
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-03 14:02:16 +0000

    touch .py for training of the transformer

---
+++ scripts/transformer_training.py
@@ -0,0 +1,12 @@
+## to be filled with transformer training ## 
+
+transformer = TransformerRegressor(
+     output_dim=output_dim,
+     param_dim=param_dim,
+     n_sources=n_sources,
+     d_model=d_model,
+     nhead=nhead,
+     num_encoder_layers=num_encoder_layers,
+     dim_feedforward=dim_feedforward,
+     dropout=dropout,
+)
