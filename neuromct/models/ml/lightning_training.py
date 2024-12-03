6824faf6a65ef08f59d68f122420ee9394e2e293
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-03 15:39:26 +0000

    fixed LightningTrainingTransformer

--- neuromct/models/ml/lightning_training.py
+++ neuromct/models/ml/lightning_training.py
@@ -1,24 +1,25 @@
 import torch.nn as nn
 import torch.optim as optim
 from lightning import LightningModule
-from neuromct.models.ml import TransformerRegressor
 
 
 class LightningTrainingTransformer(LightningModule):
     def __init__(self,
              transformer: nn.Module,
+             loss_function: nn.Module,
+             metric_function: nn.Module,
              optimizer: optim.Optimizer,
              lr_scheduler: optim.lr_scheduler.LRScheduler,
              lr: float,
-             output_dim: int,
              **kwargs,
         ):
         super(LightningTrainingTransformer, self).__init__()
         self.transformer = transformer
+        self.loss_function = loss_function
+        self.metric_function = metric_function
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
         self.lr = lr    
-        self.output_dim = output_dim
         self.kwargs = kwargs
         
         self.train_loss_to_plot = []
@@ -32,19 +33,19 @@ class LightningTrainingTransformer(LightningModule):
         return loss
 
     def _compute_and_log_val_losses(self, y_pred, y, data_type):
-        loss = self.val_loss_function(y_pred, y)
+        loss = self.metric_function(y_pred, y)
         self.log(f"{data_type}_loss", loss, prog_bar=True, on_epoch=True)
         return loss
 
     def training_step(self, batch):
-        x, y = batch[:, self.output_dim:], batch[:, :self.output_dim]
+        x, y = batch
         y_pred = self(x)
         loss = self._compute_and_log_losses(y_pred, y, "training")
         self.train_loss_to_plot.append(loss.item())
         return loss
 
-    def validation_step(self, batch, batch_idx, dataloader_idx=0):
-        x, y = batch[:, self.output_dim:], batch[:, :self.output_dim]
+    def validation_step(self, batch, dataloader_idx=0):
+        x, y = batch
         y_pred = self(x)
         if dataloader_idx == 0:
             loss = self._compute_and_log_val_losses(y_pred, y, "val1")
@@ -65,4 +66,3 @@ class LightningTrainingTransformer(LightningModule):
         opt = self.optimizer(self.parameters(), lr=self.lr, maximize=False)
         scheduler = lr_scheduler(opt, mode='min', factor=0.95, patience=5, verbose=False) # depends on lr_scheduler. Needs more flexibility
         return [opt], [{'scheduler': scheduler, 'monitor': "val_loss"}]
-
