52bc778b19d6cc8f23e88e71e10a30efe5a12526
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-03 16:43:45 +0000

    minor

--- neuromct/dataset/dataset.py
+++ neuromct/dataset/dataset.py
@@ -2,15 +2,15 @@ from torch.utils.data import Dataset
 
 
 class JMLDataset(Dataset):
-    def __init__(self, spectra, params, source_type):
+    def __init__(self, spectra, params, source_types):
         assert len(data) == len(labels) == len(parameters), \
             "All tensors must have the same length"
         self.spectra = spectra
         self.params = params
-        self.source_type = source_type
+        self.source_types = source_types
 
     def __len__(self):
         return len(self.spectra)
 
     def __getitem__(self, idx):
-        return self.spectra[idx], self.params[idx], self.source_type[idx]
+        return self.spectra[idx], self.params[idx], self.source_types[idx]
