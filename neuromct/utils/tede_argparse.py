1c8c50cb3f79ea62e1bbfaceb1a0a18d8a089430
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-06 20:03:40 +0000

    model argparser

---
+++ neuromct/utils/tede_argparse.py
@@ -0,0 +1,35 @@
+import argparse
+import json
+
+
+def tede_argparse():
+    parser = argparse.ArgumentParser(description='Train the TEDE model')
+    parser.add_argument("--config", type=str, default='neuromct/configs/tede_training_configs.json',
+                         help="The path to the JSON config file")
+    parser.add_argument("--N_resamplings", type=int, default=1000,
+                         help='The number of Poisson resamples for data augmentation')
+    parser.add_argument("--batch_size", type=int, default=32,
+                         help='The batch size (default=32). Note, that the effective batch size is batch_size * N_resamplings')
+    parser.add_argument("--lr", type=float, default=5e-4,
+                        help='The learning rate (default=5e-4)')
+    parser.add_argument("--output_dim", type=int, default=800,
+                         help='The number of bins that used to describe the spectra (default=800)')
+    parser.add_argument("--d_model", type=int, default=128,
+                         help='The number of expected features in the input of the encoder layers (default=128)')
+    parser.add_argument("--nhead", type=int, default=8,
+                         help='The number of heads in the multiheadattention models (default=8)')
+    parser.add_argument("--num_encoder_layers", type=int,
+                         help='The number of sub-encoder-layers in the encoder (default=5)')
+    parser.add_argument("--dim_feedforward", type=int, default=32,
+                         help='The dimension of the feedforward network model (default=32)')
+    parser.add_argument("--dropout", type=float, default=0.1,
+                         help='The dropout value (default=0.1)')
+    args = parser.parse_args()
+
+    if args.config:
+        with open(args.config, 'r') as f:
+            config_args = json.load(f)
+        parser.set_defaults(**config_args)
+
+    final_args = parser.parse_args()
+    return final_args
