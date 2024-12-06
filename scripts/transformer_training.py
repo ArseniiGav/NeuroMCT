de7c75b59e0e2702938f3dee072ca7c45844c7e4
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-06 20:04:19 +0000

    major update of the training script

--- scripts/transformer_training.py
+++ scripts/transformer_training.py
@@ -1,34 +1,30 @@
-import argparse
 import torch
+import torch.optim as optim
 from torchvision import transforms
 from torch.utils.data import DataLoader
-from neuromct.models.ml import TEDE
+from neuromct.models.ml import TEDE, TEDELightningTraining
+from neuromct.models.ml.losses import CosineDistanceLoss
 from neuromct.dataset import PoissonResampling, NormalizeToUnity, JMLDataset
-from neuromct.configs import configs
+from neuromct.configs import data_configs
+from neuromct.utils import tede_argparse
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.callbacks import LearningRateMonitor
 from lightning import Trainer
 
 
-parser = argparse.ArgumentParser(description='Train the TEDE model')
-parser.add_argument("--N_resamplings", type=int, default=1000, help='Number of Possion resamples for data augmentation')
-parser.add_argument("--batch_size", type=int, default=32, help='Batch size')
-parser.add_argument("--d_model", type=int, default=128, help='D model')
-parser.add_argument("--nhead", type=int, default=8, help='nhead')
-parser.add_argument("--num_encoder_layers", type=int, default=5, help='Number of encoder layers')
-parser.add_argument("--dim_feedforward", type=int, default=32, help='Dim feedforward')
-parser.add_argument("--dropout", type=float, default=0.1, help='Dropout in the encoder layers')
-args = parser.parse_args()
+args = tede_argparse()
 
 transform = transforms.Compose([
     PoissonResampling(N_resamplings=args.N_resamplings),  # Apply Poisson resampling
     NormalizeToUnity()                                    # Normalize to unity
 ])
 
-train_data = JMLDataset(dataset_type="training", path_to_processed_data=configs['path_to_processed_data'], transform=transform)
-val1_data = JMLDataset(dataset_type="val1", path_to_processed_data=configs['path_to_processed_data'])
-val2_1_data = JMLDataset(dataset_type="val2_1", path_to_processed_data=configs['path_to_processed_data'])
-val2_2_data = JMLDataset(dataset_type="val2_2", path_to_processed_data=configs['path_to_processed_data'])
-val2_3_data = JMLDataset(dataset_type="val2_3", path_to_processed_data=configs['path_to_processed_data'])
+train_data = JMLDataset(dataset_type="training", path_to_processed_data=data_configs['path_to_processed_data'], transform=transform)
+val1_data = JMLDataset(dataset_type="val1", path_to_processed_data=data_configs['path_to_processed_data'])
+val2_1_data = JMLDataset(dataset_type="val2_1", path_to_processed_data=data_configs['path_to_processed_data'])
+val2_2_data = JMLDataset(dataset_type="val2_2", path_to_processed_data=data_configs['path_to_processed_data'])
+val2_3_data = JMLDataset(dataset_type="val2_3", path_to_processed_data=data_configs['path_to_processed_data'])
 
 train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=20)
 val1_loader = DataLoader(val1_data, batch_size=val1_data.shape[0], shuffle=False)
@@ -36,15 +32,35 @@ val2_1_loader = DataLoader(val2_1_data, batch_size=val2_1_data.shape[0], shuffle
 val2_2_loader = DataLoader(val2_2_data, batch_size=val2_2_data.shape[0], shuffle=False)
 val2_3_loader = DataLoader(val2_3_data, batch_size=val2_3_data.shape[0], shuffle=False)
 
-regressor = TEDE(
-     output_dim=configs['output_dim'],
-     param_dim=configs['params_dim'],
-     n_sources=configs['n_sources'],
-     d_model=args.d_model,
-     nhead=args.nhead,
-     num_encoder_layers=args.num_encoder_layers,
-     dim_feedforward=args.dim_feedforward,
-     dropout=args.dropout,
+loss_function = CosineDistanceLoss()
+val_metric_function = CosineDistanceLoss()
+
+optimizer = optim.Adam()
+lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau()
+
+monitor_metric_es = "val_loss"
+monitor_metric_checkpoint = "val_loss"
+checkpoint_callback = ModelCheckpoint(save_top_k=1, monitor=monitor_metric_checkpoint, mode="min")
+early_stopping_callback = EarlyStopping(monitor=monitor_metric_es, mode="min", patience=50)
+
+tede_model = TEDE(
+    param_dim=data_configs['params_dim'],
+    n_sources=data_configs['n_sources'],
+    output_dim=args.output_dim,
+    d_model=args.d_model,
+    nhead=args.nhead,
+    num_encoder_layers=args.num_encoder_layers,
+    dim_feedforward=args.dim_feedforward,
+    dropout=args.dropout,
+)
+
+tede_model_lightning_training = TEDELightningTraining(
+    model=tede_model,
+    loss_function=loss_function,
+    val_metric_function=val_metric_function,
+    optimizer=optimizer,
+    lr_scheduler=lr_scheduler,
+    lr=args.lr
 )
 
 trainer_regressor = Trainer(
@@ -62,7 +78,7 @@ trainer_regressor = Trainer(
 )
 
 trainer_regressor.fit(
-    regressor,
+    tede_model_lightning_training,
     train_dataloaders=train_loader,
     val_dataloaders=[
       val1_loader,
@@ -76,9 +92,9 @@ print(checkpoint_callback.best_model_path)
 print(checkpoint_callback.best_model_score)
 best_model = TEDE.load_from_checkpoint(
     checkpoint_callback.best_model_path,
-    output_dim=configs['output_dim'],
-    param_dim=configs['params_dim'],
-    n_sources=configs['n_sources'],
+    param_dim=data_configs['params_dim'],
+    n_sources=data_configs['n_sources'],
+    output_dim=args.output_dim,
     d_model=args.d_model,
     nhead=args.nhead,
     num_encoder_layers=args.num_encoder_layers,
@@ -87,4 +103,4 @@ best_model = TEDE.load_from_checkpoint(
 )
 
 # Save the regressor model
-torch.save(best_model.state_dict(), f"{configs['path_to_saved_models']}/Regressor.pth")
+torch.save(best_model.state_dict(), f"{data_configs['path_to_saved_models']}/Regressor.pth")
