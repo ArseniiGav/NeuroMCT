92417560c870af711ce0bae2a02a9e0d17bb3b9f
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-08 22:09:38 +0000

    version 0.0.2: re-factored the code for the tede model

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="neuromct",
-    version="0.0.1",
+    version="0.0.2",
     author='Arsenii Gavrikov, Andrea Serafini',
     author_email="arsenii.gavrikov@pd.infn.it, andrea.serafini@pd.infn.it",
     packages=find_packages(),
