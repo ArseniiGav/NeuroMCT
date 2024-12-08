06911994a76d3c5d6584e235af1168739da8099e
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-08 18:03:51 +0000

    batch size first = true

--- neuromct/models/ml/transformer.py
+++ neuromct/models/ml/transformer.py
@@ -17,13 +17,23 @@ class TEDE(nn.Module):
 
         self.param_emb_layer = nn.Linear(param_dim, d_model // 2)
         self.source_type_emb_layer = nn.Embedding(n_sources, d_model // 2)
-        self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward, dropout=dropout)
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_encoder_layers)
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model,
+            nhead=nhead,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            batch_first=True
+        )
+        self.transformer_encoder = nn.TransformerEncoder(
+            self.encoder_layer,
+            num_layers=num_encoder_layers
+        )
+        
         self.regression_head = nn.Sequential(
             nn.Linear(d_model, d_model),
             nn.GELU(),
             nn.Linear(d_model, output_dim),
-            nn.Softmax()
+            nn.Softmax(dim=1)
         )
         self.apply(self._init_weights)
 
@@ -37,8 +47,8 @@ class TEDE(nn.Module):
 
     def forward(self, params, source_types):
         param_emb = self.param_emb_layer(params)
-        source_type_emb = self.source_type_emb_layer(source_types)
-        x = torch.cat([param_emb, source_type_emb], dim=1)
+        source_type_emb = self.source_type_emb_layer(source_types).squeeze(dim=1) # collapse the label dim
+        x = torch.cat((param_emb, source_type_emb), dim=1)
         x = self.transformer_encoder(x)
         spectra = self.regression_head(x)
         return spectra
