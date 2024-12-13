46629ecbee33de9388e2a6df4d40b2ebc36316b9
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-13 23:38:27 +0000

    add logscale transform

--- neuromct/dataset/transformations.py
+++ neuromct/dataset/transformations.py
@@ -2,14 +2,24 @@ import torch
 
 
 class PoissonNoise:
-    def __call__(self, spectra):
+    def __call__(self, x):
         # Apply Poisson noise
-        spectra_noised = torch.poisson(spectra)
-        return spectra_noised
+        x_noised = torch.poisson(x)
+        return x_noised
 
 
 class NormalizeToUnity:
-    def __call__(self, spectra):
-        # Normalize spectra to get a pdf
-        pdf = spectra / torch.sum(spectra)
-        return pdf
+    def __call__(self, x):
+        # Normalize x to unity (e.g. normalize spectra to get a PDF)
+        x_normalized = x / torch.sum(x)
+        return x_normalized
+
+
+class LogScale:
+    def __init__(self, ε=1e-8):
+        self.ε = ε
+
+    def __call__(self, x):
+        # Apply the log rescaling transformation
+        x_log_scale = torch.log(x + self.ε)
+        return x_log_scale
