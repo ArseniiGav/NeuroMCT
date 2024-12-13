da48cf28dae831ef7d60791ea9bb8030c7c00530
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-13 00:46:40 +0000

    integrate visualizer with lightning model

--- neuromct/models/ml/lightning_training.py
+++ neuromct/models/ml/lightning_training.py
@@ -31,29 +31,26 @@ class TEDELightningTraining(LightningModule):
         self.kwargs = kwargs
         if kwargs['res_visualizator']:
             self.res_visualizator = kwargs['res_visualizator']
+
             self.val1_params_to_vis = self.res_visualizator.val1_data_to_vis[1]
             self.val1_source_types_to_vis = self.res_visualizator.val1_data_to_vis[2]
 
-    def _compute_and_log_losses(self, spectra_predict, spectra_true, data_type):
-        loss = self.loss_function(spectra_predict, spectra_true)
-        self.log(f"{data_type}_loss", loss, prog_bar=True)
-        return loss
+            self.val2_params_to_vis = self.res_visualizator.val2_params_to_vis
+            self.val2_source_types_to_vis = self.res_visualizator.val2_source_types_to_vis
 
-    def _compute_and_log_val_losses(self, spectra_predict, spectra_true, data_type):
-        loss = self.val_metric_function(spectra_predict, spectra_true)
-        self.log(f"{data_type}_loss", loss, prog_bar=True, on_epoch=True)
-        return loss
+    def forward(self, params, source_types):
+        return self.model(params, source_types)
 
     def training_step(self, batch):
         spectra_true, params, source_types = batch
-        spectra_predict = self.model(params, source_types)
+        spectra_predict = self(params, source_types)
         loss = self._compute_and_log_losses(spectra_predict, spectra_true, "training")
         self.train_loss_to_plot.append(loss.item())
         return loss
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
         spectra_true, params, source_types = batch
-        spectra_predict = self.model(params, source_types)
+        spectra_predict = self(params, source_types)
         if dataloader_idx == 0:
             loss = self._compute_and_log_val_losses(spectra_predict, spectra_true, "val1")
             self.val1_epoch_loss = loss.item()
@@ -69,28 +66,47 @@ class TEDELightningTraining(LightningModule):
         self.val_loss_to_plot.append(self.val_epoch_loss)
         self.log('val_loss', self.val_epoch_loss, prog_bar=True)
 
-        self.model.eval()
+        self.eval()
         with torch.no_grad():
-            val1_spectra_pdf_to_vis = self.model(
-                self.val1_params_to_vis, self.val1_source_types_to_vis
+            val1_spectra_pdfs_to_vis = []
+            for i in range(len(self.val1_params_to_vis)):
+                val1_spectra_pdf_to_vis = self(
+                    self.val1_params_to_vis[i].to(self.device),
+                    self.val1_source_types_to_vis[i].to(self.device)
+                ).detach().cpu().numpy()
+                val1_spectra_pdfs_to_vis.append(val1_spectra_pdf_to_vis)
+
+            val2_spectra_pdfs_to_vis = self(
+                self.val2_params_to_vis.to(self.device),
+                self.val2_source_types_to_vis.to(self.device)
             ).detach().cpu().numpy()
-        self.model.train()
+        self.train()
 
         self.res_visualizator.plot_val1_spectra(
-            spectra_pdf_to_vis=val1_spectra_pdf_to_vis,
+            spectra_pdf_to_vis=val1_spectra_pdfs_to_vis,
             current_epoch=self.current_epoch,
             global_step=self.global_step,
             val1_metric=self.val1_epoch_loss,
         )
 
-        # self.res_visualizator.plot_val2_spectra(
-        #     spectra_pdf_to_vis=val2_spectra_pdf_to_vis,
-        #     current_epoch=self.current_epoch,
-        #     global_step=self.global_step,
-        #     val1_metric=self.val2_epoch_loss,
-        # )
+        self.res_visualizator.plot_val2_spectra(
+            spectra_pdf_to_vis=val2_spectra_pdfs_to_vis,
+            current_epoch=self.current_epoch,
+            global_step=self.global_step,
+            val2_metric=self.val2_epoch_loss,
+        )
 
     def configure_optimizers(self):      
         opt = self.optimizer(self.parameters(), lr=self.lr, maximize=False)
         scheduler = self.lr_scheduler(opt, mode='min', factor=0.95, patience=10, verbose=False) # depends on lr_scheduler. Needs more flexibility
         return [opt], [{'scheduler': scheduler, 'monitor': "val_loss"}]
+
+    def _compute_and_log_losses(self, spectra_predict, spectra_true, data_type):
+        loss = self.loss_function(spectra_predict, spectra_true)
+        self.log(f"{data_type}_loss", loss, prog_bar=True)
+        return loss
+
+    def _compute_and_log_val_losses(self, spectra_predict, spectra_true, data_type):
+        loss = self.val_metric_function(spectra_predict, spectra_true)
+        self.log(f"{data_type}_loss", loss, prog_bar=True, on_epoch=True)
+        return loss
