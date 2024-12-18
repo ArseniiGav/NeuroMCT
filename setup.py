871237cd91a8f5a0fa0161740c25ca8f77cfd525
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-18 08:39:58 +0000

    add package data

--- setup.py
+++ setup.py
@@ -7,4 +7,10 @@ setup(
     author_email="arsenii.gavrikov@pd.infn.it, andrea.serafini@pd.infn.it",
     packages=find_packages(),
     package_dir={"neuromct": "neuromct"},
+    include_package_data=True,
+    package_data={
+        "neuromct": [
+            "configs/*",
+        ]
+    },  # directory which contains your data
 )
