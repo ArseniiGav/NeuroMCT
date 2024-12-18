0d714b6bdb449d7bd55ed0f065601e6c86e8dedd
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-18 08:57:30 +0000

    remove wd

--- neuromct/models/ml/losses/regression_losses.py
+++ neuromct/models/ml/losses/regression_losses.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from neuromct.configs import data_configs
-from ot.lp import wasserstein_1d
 
 
 class CosineDistanceLoss(nn.Module):
@@ -26,45 +24,3 @@ class GeneralizedPoissonNLLLoss(nn.Module):
         else:
             output = nn.PoissonNLLLoss(log_input=False)
         return output
-
-
-class WassersteinLoss(nn.Module):
-    def __init__(self, hist_based=False):
-        super(WassersteinLoss, self).__init__()
-        self.hist_based = hist_based
-        if self.hist_based:
-            kNPE_bins_edges = torch.tensor(
-                data_configs["kNPE_bins_edges"], dtype=torch.float64)
-            self.kNPE_bins_centers = (kNPE_bins_edges[1:] + kNPE_bins_edges[:-1]) / 2
-
-    def forward(self, spectra_predict, spectra_true):
-        # Verify data consistency
-        if spectra_predict.shape[0] != spectra_true.shape[0]:
-            raise ValueError("Mismatch in the batch sizes between predicted and true spectra.")
-        elif spectra_predict.shape[1] != spectra_true.shape[1]:
-            raise ValueError("Mismatch in the binning sizes between predicted and true spectra.")
-
-        original_deterministic_setting = torch.are_deterministic_algorithms_enabled()
-        if self.hist_based:
-            batch_size = spectra_true.shape[0]
-            self.kNPE_bins_centers_repeated = self.kNPE_bins_centers.repeat((batch_size, 1)).T
-
-            #torch.cumsum used in wasserstein_1d from POT does not have a deterministic implementation            
-            torch.use_deterministic_algorithms(False)
-            loss = wasserstein_1d(
-                self.kNPE_bins_centers_repeated.to(spectra_predict.device),
-                self.kNPE_bins_centers_repeated.to(spectra_predict.device),
-                spectra_predict.T,
-                spectra_true.T
-            )
-            torch.use_deterministic_algorithms(original_deterministic_setting)
-            return torch.mean(loss)
-        else:
-            #torch.cumsum used in wasserstein_1d from POT does not have a deterministic implementation
-            torch.use_deterministic_algorithms(False)
-            loss = wasserstein_1d(
-                spectra_predict.T,
-                spectra_true.T
-            )
-            torch.use_deterministic_algorithms(original_deterministic_setting)
-            return torch.mean(loss)
