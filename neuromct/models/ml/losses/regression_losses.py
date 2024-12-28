4b756e9075662a0cab0976708b6632a96e63ffac
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2024-12-28 15:30:20 +0000

    add kl-div loss

--- neuromct/models/ml/losses/regression_losses.py
+++ neuromct/models/ml/losses/regression_losses.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class CosineDistanceLoss(nn.Module):
     def __init__(self):
         super(CosineDistanceLoss, self).__init__()
@@ -24,3 +23,29 @@ class GeneralizedPoissonNLLLoss(nn.Module):
         else:
             output = nn.PoissonNLLLoss(log_input=False)
         return output
+
+class GeneralizedKLDivLoss(nn.Module):
+    def __init__(self, log_input, log_target, reduction, eps=1e-9):
+        """
+        Initialize the KL-divergence loss.
+        :param reduction: Specifies the reduction to apply to the output.
+                          Options: 'batchmean', 'sum', 'mean', or 'none'.
+                log: Specifies the scale of predicted input.
+        """
+        super(GeneralizedKLDivLoss, self).__init__()
+        self.eps = eps
+        self.log_input = log_input
+        self.kl_div = nn.KLDivLoss(reduction=reduction, log_target=log_target)
+
+    def forward(self, predicted, target):
+        """
+        Compute the KL-divergence loss.
+        :param predicted: Predicted probabilities (Q).
+        :param target: Target probabilities (P).
+        :return: KL-divergence loss.
+        """
+        if self.log_input:
+            kl_div_loss = self.kl_div(predicted, target)
+        else:
+            kl_div_loss = self.kl_div(torch.log(predicted + self.eps), target)
+        return kl_div_loss
