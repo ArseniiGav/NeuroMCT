3a11b2f8763ce1e2ce5549a3d198c462b16b96d7
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2025-01-03 01:57:44 +0000

    move to models.ml and add reduction

--- neuromct/models/ml/metrics/metrics.py
+++ neuromct/models/ml/metrics/metrics.py
@@ -11,42 +11,45 @@ import torch.nn as nn
 
 
 class LpNormDistance(nn.Module):
-    r"""
-    The module to compute the Lp-norm distance between two one-dimensional probability distributions.
-    Parameters
-        - p : int, float, or torch.inf, optional (default=2)
-            The order of the norm:
-            - If p = 1, computes the Wasserstein distance.
-            - If p = 2, computes the Cramér-von Mises distance.
-            - If p = torch.inf, computes the Kolmogorov-Smirnov distance.
-            - if p is a positive float computer the general Lp distance.
-    Methods
-        - forward(u_values, v_values, u_weights=None, v_weights=None)
-            Compute Lp-norm distance between two one-dimensional probability distributions u and v,
-            whose respective CDFs are U and V. Where the Lp-norm distance that is defined as follows:
+    def __init__(self, p=2, reduction='mean'):
+        r"""
+        The Pytorch module to compute the Lp-norm distance 
+        between two one-dimensional probability distributions.
 
-            .. math::
+        Parameters
+            - p : int, float, or torch.inf, optional (default=2)
+                The order of the norm:
+                - If p = 1, computes the Wasserstein distance.
+                - If p = 2, computes the Cramér-von Mises distance.
+                - If p = torch.inf, computes the Kolmogorov-Smirnov distance.
+                - if p is a positive float computer the general Lp distance.
+        Methods
+            - forward(u_values, v_values, u_weights=None, v_weights=None)
+                Computes Lp-norm distance between two one-dimensional probability distributions u and v,
+                whose respective CDFs are U and V. Where the Lp-norm distance that is defined as follows:
 
-                l_p(u, v) = \left( \int_{-\infty}^{+\infty} |U-V|^p \right)^{1/p}
+                .. math::
 
-            p is a positive parameter; p = 1 gives the Wasserstein distance, p = 2
-            gives the Cramér-von Mises distance.
+                    l_p(u, v) = \left( \int_{-\infty}^{+\infty} |U-V|^p \right)^{1/p}
 
-            Parameters
-            ----------
-            u_values, v_values: array_like
-                Values observed in the (empirical) distribution.
-            u_weights, v_weights: array_like, optional
-                Weight for each value. If unspecified, each value is assigned the same weight.
-                u_weights (resp. v_weights) must have the same length as u_values (resp. v_values). 
+                p is a positive parameter; p = 1 gives the Wasserstein distance, p = 2
+                gives the Cramér-von Mises distance.
 
-            Returns
-            -------
-            distance : float
-                The computed distance between the distributions.
-    """
-    def __init__(self, p=2):
+                Parameters
+                ----------
+                u_values, v_values: array_like
+                    Values observed in the (empirical) distribution.
+                u_weights, v_weights: array_like, optional
+                    Weight for each value. If unspecified, each value is assigned the same weight.
+                    u_weights (resp. v_weights) must have the same length as u_values (resp. v_values). 
+
+                Returns
+                -------
+                distance : float
+                    The computed distance between the distributions.
+        """
         super().__init__()
+        self.reduction = reduction
         self.p = torch.tensor(p)
         if torch.isfinite(self.p) and self.p <= 0:
             raise ValueError("p must be positive: p > 0.")
@@ -92,6 +95,18 @@ class LpNormDistance(nn.Module):
         # Get the differences between CDFs and the the differences between the corresponding values
         cdfs_deltas, x_deltas = self._get_cdf_diffs(u_values, v_values, u_weights, v_weights)
         if torch.isfinite(self.p) and self.p > 0:
-            return torch.pow(torch.sum((cdfs_deltas ** self.p) * x_deltas, dim=1), 1 / self.p)
+            distance = torch.pow(torch.sum((cdfs_deltas ** self.p) * x_deltas, dim=1), 1 / self.p)
         else:
-            return torch.max(cdfs_deltas, dim=1).values
+            distance = torch.max(cdfs_deltas, dim=1).values
+        
+        if self.reduction == "mean":
+            # normalize by the number of samples in the batch
+            return distance.sum() / u_values.size(0)
+        elif self.reduction == "sum":
+             # sum the distances over the batch
+            return distance.sum()
+        elif self.reduction == "none":
+            # no reduction
+            return distance
+        else:
+            raise ValueError("reduction must be one of 'mean', 'sum', or 'none'.")
