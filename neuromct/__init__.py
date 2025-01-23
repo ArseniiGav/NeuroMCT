3de8c6b9d3954eac06965dbe7b179e7928d0058c
Dmitry Dolzhikov <dolzhikov.d.a@gmail.com>
2025-01-23 11:53:48 +0300

    minor

--- neuromct/__init__.py
+++ neuromct/__init__.py
@@ -1,7 +1,7 @@
 from .configs import __init__
 from .dataset import __init__
 from .models import __init__
-from .orsa_fit import __init__
+# from .orsa_fit import __init__
 from .utils import __init__
 from .plot import __init__
 from .fit import __init__
