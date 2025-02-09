65db0237b64abd42aa9447407daf8fb1cb79de64
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2025-02-09 17:08:10 +0000

    add new author

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="neuromct",
     version="0.0.2",
-    author='Arsenii Gavrikov, Andrea Serafini',
-    author_email="arsenii.gavrikov@pd.infn.it, andrea.serafini@pd.infn.it",
+    author='Arsenii Gavrikov, Andrea Serafini, Dmitry Dolzhikov',
+    author_email="arsenii.gavrikov@pd.infn.it",
     packages=find_packages(),
     package_dir={"neuromct": "neuromct"},
     include_package_data=True,
