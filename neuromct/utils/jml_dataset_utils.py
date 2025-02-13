4b8d967517155290603a2ecd95b119ab0be4cb72
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2025-02-13 17:08:00 +0000

    norm to pdf

--- neuromct/utils/jml_dataset_utils.py
+++ neuromct/utils/jml_dataset_utils.py
@@ -2,7 +2,7 @@ from torchvision import transforms
 
 from ..dataset import JMLDataset
 from ..dataset import PoissonNoise
-from ..dataset import NormalizeToUnity
+from ..dataset import BuildPDF
 
 def create_dataset(dataset_type, path_to_processed_data, transform=False, val2_rates=False):
     return JMLDataset(
@@ -12,14 +12,14 @@ def create_dataset(dataset_type, path_to_processed_data, transform=False, val2_r
         val2_rates=val2_rates
     )
 
-def define_transformations(dataset_type):
+def define_transformations(dataset_type, Δx):
     if dataset_type == 'training':
         data_transform = transforms.Compose([
-            PoissonNoise(),     # Apply Poisson resampling
-            NormalizeToUnity()  # Normalize to unity
+            PoissonNoise(), # Apply Poisson resampling
+            BuildPDF(Δx)  # Build a PDF
         ])
     elif dataset_type == 'val':
         data_transform = transforms.Compose([
-            NormalizeToUnity()  # Normalize to unity
+            BuildPDF(Δx)  # Build a PDF
         ])
     return data_transform
