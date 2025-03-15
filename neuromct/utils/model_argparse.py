9f8a6e105d10158d2c9d3e40fd7ee57de8d22a7e
Arsenii Gavrikov <gavrikov.997755@gmail.com>
2025-03-15 22:57:33 +0000

    minor

--- neuromct/utils/model_argparse.py
+++ neuromct/utils/model_argparse.py
@@ -12,12 +12,12 @@ def tede_argparse():
                          help='The number of calibration sources (default=5).')
     parser.add_argument("--batch_size", type=int, default=32,
                          help='The batch size (default=32).')
-    parser.add_argument("--lr", type=float, default=5e-4,
+    parser.add_argument("--learning_rate", type=float, default=5e-4,
                         help='The learning rate (default=5e-4)')
     parser.add_argument("--output_dim", type=int, default=800,
                          help='The number of bins that used to describe the spectra (default=800)')
     parser.add_argument("--activation_function", type=str, default="relu",
-                         help='Activation function of the hidden layers (Either "relu" or "gelu", default="relu")')
+                         help='Activation function of the hidden layers. Either "relu" or "gelu"; (default="relu")')
     parser.add_argument("--d_model", type=int, default=128,
                          help='The number of expected features in the input of the encoder layers (default=128)')
     parser.add_argument("--nhead", type=int, default=8,
@@ -27,7 +27,7 @@ def tede_argparse():
     parser.add_argument("--dim_feedforward", type=int, default=32,
                          help='The dimension of the feedforward network model (default=32)')
     parser.add_argument("--dropout", type=float, default=0.1,
-                         help='The dropout value in the Transformer encoder layers (default=0.1)')
+                         help='The dropout prob. value in the Transformer encoder layers (default=0.1)')
     parser.add_argument("--temperature", type=float, default=1.0,
                          help='The temperature parameter for the output Softmax layer (default=1.0)')
     parser.add_argument("--monitor_metric", type=str, default="val_cramer_metric",
@@ -53,18 +53,21 @@ def nfde_argparse():
                          help="The path to the JSON config file")
     parser.add_argument("--n_flows", type=int, default=25,
                          help='The number of flows (default=25).')
-    parser.add_argument("--batch_size", type=int, default=32,
-                         help='The batch size (default=32).')
-    parser.add_argument("--lr", type=float, default=5e-4,
+    parser.add_argument("--batch_size", type=int, default=1,
+                         help='The batch size (default=1).')
+    parser.add_argument("--learning_rate", type=float, default=5e-4,
                         help='The learning rate (default=5e-4)')
     parser.add_argument("--activation_function", type=str, default="relu",
-                         help='Activation function of the hidden layers (Either "relu" or "gelu", default="relu")')
+                        help='Activation function of the hidden layers. Either "relu" or "gelu"; (default="relu")')
+    parser.add_argument("--activation_function", type=str, default="kl-div",
+                         help="""Loss function to train the model.
+                                 Either "kl-div", "wasserstein", or "cramer" (default="kl-div")""")
     parser.add_argument("--n_units", type=int, default=20,
                          help="The number of units in a flow's conditional network (default=20)")
     parser.add_argument("--flow_type", type=str, default="planar",
                          help='The type of the flows (default=planar)')
-    parser.add_argument("--base_type", type=str, default="normal",
-                         help='The type of the base distribution (default=normal)')
+    parser.add_argument("--dropout", type=float, default=0.0,
+                         help="The dropout prob. value in the Flow's networks (default=0.0)")
     parser.add_argument("--monitor_metric", type=str, default="val_cramer_metric",
                         help='''The main validation metric used during the training
                                 (i.e., used for the early stopping condition). Can be val_*_metric, 
