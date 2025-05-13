ec9afba910a383c00c31e7a7f8d72cf97bde4bf4
dmitryd <dolzhikov.d.a@gmail.com>
2025-05-13 18:28:32 +0300

    Add new TEDE

--- neuromct/models/ml/tede.py
+++ neuromct/models/ml/tede.py
@@ -63,15 +63,23 @@ class TEDE(nn.Module):
         ):
         super(TEDE, self).__init__()
         self.bin_size = bin_size
+        self.d_model = d_model
 
+        # self.param_emb_embedding = nn.Sequential(
+        #     nn.Linear(1, d_model // 4),
+        #     #nn.LayerNorm(d_model // 4),
+        #     nn.ReLU() if activation == 'relu' else nn.GELU(),
+        #     nn.Linear(d_model // 4, d_model // 2),
+        #     #nn.LayerNorm(d_model // 2),
+        #     nn.ReLU() if activation == 'relu' else nn.GELU(),
+        #     nn.Linear(d_model // 2, d_model),
+        # )
         self.param_emb_embedding = nn.Sequential(
-            nn.Linear(1, d_model // 4),
-            nn.LayerNorm(d_model // 4),
-            nn.ReLU() if activation == 'relu' else nn.GELU(),
-            nn.Linear(d_model // 4, d_model // 2),
-            nn.LayerNorm(d_model // 2),
+            nn.Linear(3, d_model // 2),
             nn.ReLU() if activation == 'relu' else nn.GELU(),
             nn.Linear(d_model // 2, d_model),
+            nn.ReLU() if activation == 'relu' else nn.GELU(),
+            nn.Linear(d_model, d_model * 3),
         )
         self.source_type_embedding = nn.Embedding(n_sources, d_model)
 
@@ -91,10 +99,10 @@ class TEDE(nn.Module):
 
         self.regression_head = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(d_model * 4, output_dim // 2), # * 4 stands for the number of parameters and source type
-            nn.LayerNorm(output_dim // 2),
+            nn.Linear(d_model * 4, output_dim), # * 4 stands for the number of parameters and source type
+            nn.LayerNorm(output_dim),
             nn.ReLU() if activation == 'relu' else nn.GELU(),
-            nn.Linear(output_dim // 2, output_dim),
+            nn.Linear(output_dim, output_dim),
             Softmax(temperature=temperature, dim=1)
         )
 
@@ -123,8 +131,8 @@ class TEDE(nn.Module):
         4. Project to output dimension
         5. Apply temperature-scaled Softmax for normalization
         """
-        params = params.unsqueeze(2) # [B, param_dim] -> [B, param_dim, 1]
-        param_emb = self.param_emb_embedding(params) # [B, param_dim, 1] -> [B, param_dim, d_model]
+        param_emb = self.param_emb_embedding(params)  # [B, param_dim] -> [B, param_dim*d_model]
+        param_emb = param_emb.reshape(-1, params.shape[1], self.d_model)  # [B, param_dim*d_model] -> [B, param_dim, d_model]
         source_type_emb = self.source_type_embedding(source_types) #  [B, 1] -> [B, 1, d_model]
         embs_cat = torch.cat((param_emb, source_type_emb), dim=1) # [B, param_dim+1, d_model]
         x = self.transformer_encoder(embs_cat) # [B, param_dim+1, d_model]
